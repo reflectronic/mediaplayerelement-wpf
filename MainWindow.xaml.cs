7e5ef9f673767a37d478cf675b430f57dc42b718
reflectronic <john-tur@outlook.com>
2022-09-23 02:19:25 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Graphics.Imaging;
+using Windows.Media.Core;
+using Windows.Win32;
+using Windows.Win32.Graphics.Dxgi;
+
+using WinRT;
+using SharpDX.Direct3D11;
+
+namespace WpfApp1;  
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        PlayerElement.MediaPlayer.Source = MediaSource.CreateFromUri(new(PlaybackUri.Text));
+        PlayerElement.MediaPlayer.MediaFailed += (_, e) => Dispatcher.Invoke(() => MessageBox.Show(e.ToString()));
+        PlayerElement.MediaPlayer.Play();
+    }
+
+    private async void Button2_Click(object sender, RoutedEventArgs e)
+    {
+        var d3d11Device = new Device(SharpDX.Direct3D.DriverType.Hardware, DeviceCreationFlags.BgraSupport);
+
+        var texture = new Texture2D(d3d11Device, new Texture2DDescription()
+        {
+            Width = 640,
+            Height = 490,
+            ArraySize = 1,
+            Usage = ResourceUsage.Staging,
+            CpuAccessFlags = CpuAccessFlags.Read,
+            Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
+            MipLevels = 1,
+            SampleDescription =
+            {
+                Count = 1
+            }
+        });
+
+        var dxgiSurface = texture.QueryInterface<SharpDX.DXGI.Surface>();
+        var pDxgiSurface = dxgiSurface.NativePointer;
+        PInvoke.CreateDirect3D11SurfaceFromDXGISurface((IDXGISurface)Marshal.GetObjectForIUnknown(pDxgiSurface), out var inspectable).ThrowOnFailure();
+        GC.KeepAlive(texture);
+
+        var pUnk = Marshal.GetIUnknownForObject(inspectable);
+        var direct3dSurface = MarshalInterface<IDirect3DSurface>.FromAbi(pUnk);
+        Marshal.Release(pUnk);
+
+        PlayerElement.MediaPlayer.CopyFrameToVideoSurface(direct3dSurface);
+        var bitmap = await SoftwareBitmap.CreateCopyFromSurfaceAsync(direct3dSurface);
+
+        unsafe 
+        {
+            using var buffer = bitmap.LockBuffer(BitmapBufferAccessMode.Read);
+            var plane0 = buffer.GetPlaneDescription(0);
+
+            var access = buffer.CreateReference().As<Windows.Win32.System.WinRT.IMemoryBufferByteAccess>();
+            access.GetBuffer(out var data, out _);
+
+            var image = BitmapSource.Create(bitmap.PixelWidth,
+                bitmap.PixelHeight,
+                bitmap.DpiX,
+                bitmap.DpiY,
+                System.Windows.Media.PixelFormats.Bgra32,
+                null,
+                (nint)(data + plane0.StartIndex),
+                plane0.Width * plane0.Height * sizeof(int),
+                plane0.Stride);
+
+            CapturedImage.Source = image;
+        }
+
+    }
+}
---
+++ MediaPlayerElement.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+using Windows.UI.Composition;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.System.WinRT;
+using Windows.Win32.System.WinRT.Composition;
+using Windows.System;
+using System.Diagnostics.CodeAnalysis;
+using WinRT;
+using System.Windows;
+using Windows.Media.Playback;
+
+namespace WpfApp1;
+
+public sealed class MediaPlayerElement : HwndHost
+{
+    private IntPtr hwndHost;
+    private DpiScale scale;
+    private DispatcherQueueController? dispatcherQueue;
+
+    private CompositionTarget compositionTarget = default!;
+    private SpriteVisual videoVisual = default!;
+
+    public MediaPlayer MediaPlayer { get; } = new();
+
+    public Compositor? Compositor { get; private set; }
+
+    private void SetChild(Visual value)
+    {
+        if (Compositor == null)
+        {
+            InitComposition(hwndHost);
+        }
+
+        compositionTarget.Root = value;
+    }
+
+    public MediaPlayerElement()
+    {
+        SizeChanged += CompositionHost_SizeChanged;
+        Loaded += CompositionHost_Loaded;
+        PresentationSource.AddSourceChangedHandler(this, PresentationSourceChangedHandler);
+    }
+    private void CompositionHost_Loaded(object sender, RoutedEventArgs e)
+    {
+        videoVisual = Compositor!.CreateSpriteVisual();
+        SetChild(videoVisual);
+        Resize((int)ActualWidth, (int)ActualHeight);
+    }
+
+    private void PresentationSourceChangedHandler(object sender, SourceChangedEventArgs args)
+    {
+        if (args.NewSource != null)
+        {
+            var source = (HwndSource)args.NewSource;
+            scale = System.Windows.Media.VisualTreeHelper.GetDpi(source.RootVisual);
+        }
+    }
+
+    private void CompositionHost_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        Resize((int)e.NewSize.Width, (int)e.NewSize.Height);
+    }
+
+    protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+    {
+        scale = newDpi;
+        Resize((int)ActualWidth, (int)ActualHeight);
+        base.OnDpiChanged(oldDpi, newDpi);
+    }
+
+    private void Resize(int width, int height)
+    {
+        if (compositionTarget is { Root: not null })
+        {
+            var scaledWidth = width * scale.DpiScaleX;
+            var scaledHeight = height * scale.DpiScaleY;
+            MediaPlayer.SetSurfaceSize(new(scaledWidth, scaledHeight));
+
+            var surface = MediaPlayer.GetSurface(Compositor);
+            var brush = Compositor!.CreateSurfaceBrush(surface.CompositionSurface);
+            var oldBrush = videoVisual.Brush;
+            videoVisual.Brush = brush;
+            oldBrush?.Dispose();
+
+            compositionTarget.Root.Size = new((float)scaledWidth, (float)scaledHeight);
+        }
+    }
+
+    protected override unsafe HandleRef BuildWindowCore(HandleRef hwndParent)
+    {
+        // Create Window
+        hwndHost = IntPtr.Zero;
+        hwndHost = PInvoke.CreateWindowEx(0,
+            "static",
+            "",
+            WINDOW_STYLE.WS_CHILD | WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CLIPCHILDREN,
+            0,
+            0,
+            0,
+            0,
+            (HWND)hwndParent.Handle,
+            null,
+            null,
+            null);
+
+        dispatcherQueue = InitializeCoreDispatcher();
+
+        InitComposition(hwndHost);
+
+        return new HandleRef(this, hwndHost);
+    }
+
+    protected override void DestroyWindowCore(HandleRef hwnd)
+    {
+        compositionTarget.Root?.Dispose();
+
+        PInvoke.DestroyWindow((HWND)hwnd.Handle);
+    }
+
+    private static unsafe DispatcherQueueController InitializeCoreDispatcher()
+    {
+        var options = new DispatcherQueueOptions
+        {
+            dwSize = (uint)sizeof(DispatcherQueueOptions),
+            apartmentType = DISPATCHERQUEUE_THREAD_APARTMENTTYPE.DQTAT_COM_STA,
+            threadType = DISPATCHERQUEUE_THREAD_TYPE.DQTYPE_THREAD_CURRENT
+        };
+
+        PInvoke.CreateDispatcherQueueController(options, out DispatcherQueueController queue);
+        return queue;
+    }
+
+    [MemberNotNull(nameof(Compositor))]
+    [MemberNotNull(nameof(compositionTarget))]
+    private void InitComposition(IntPtr hwndHost)
+    {
+        Compositor = new Compositor();
+        var interop = this.Compositor.As<ICompositorDesktopInterop>();
+        interop.CreateDesktopWindowTarget((HWND)hwndHost, true, out var target);
+        compositionTarget = target;
+    }
+}
